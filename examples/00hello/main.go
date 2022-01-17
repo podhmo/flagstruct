ddffabacbd37131092537cd1e9346deebd63a24b
podhmo <ababjam61+github@gmail.com>
2022-01-17 15:17:35 +0900

    docs: readme refs #4

---
+++ examples/00hello/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/podhmo/structflag"
+)
+
+func main() {
+	type Options struct {
+		Name    string `flag:"name" help:"name of greeting"`
+		Verbose bool   `flag:"verbose" short:"v"`
+	}
+	options := &Options{Name: "foo"} // default value
+
+	b := structflag.NewBuilder()
+	b.Name = "hello"
+	b.EnvPrefix = "X_"
+
+	fs := b.Build(options)
+	fs.Parse(os.Args[1:])
+
+	fmt.Printf("parsed: %#+v\n", options)
+
+}
---
+++ examples/go.mod
@@ -0,0 +1,9 @@
+module github.com/podhmo/structflag/examples
+
+go 1.17
+
+replace github.com/podhmo/structflag => ../
+
+require github.com/podhmo/structflag v0.0.0-00010101000000-000000000000
+
+require github.com/spf13/pflag v1.0.5 // indirect
