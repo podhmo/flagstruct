e3bfbe0620d92b475064361cb2af010c99bc4914
podhmo <ababjam61+github@gmail.com>
2022-01-27 10:39:34 +0900

    chore: json-tag example

---
+++ examples/01json-tag/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/podhmo/structflag"
+)
+
+type Options struct {
+	Name    string `json:"name"`
+	Verbose bool   `json:"verbose" short:"v"`
+}
+
+func main() {
+	options := &Options{Name: "foo"} // default value
+
+	b := structflag.NewBuilder()
+	b.Name = "hello"
+	b.EnvPrefix = "X_"
+	b.FlagnameTag = "json"
+
+	fs := b.Build(options)
+	fs.Parse(os.Args[1:])
+
+	fmt.Printf("parsed: %#+v\n", options)
+
+}
