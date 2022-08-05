11eec20a420ab92955e1bbab08bb353871bf03a4
podhmo <ababjam61+github@gmail.com>
2022-08-05 20:23:17 +0900

    chore: add example using Build()

---
+++ examples/05with-continue-on-error/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/podhmo/flagstruct"
+)
+
+type Options struct {
+	Value   int  `flag:"value"`
+	Verbose bool `flag:"verbose" short:"v"`
+}
+
+func main() {
+	options := &Options{Value: 10} // default value
+	fs := flagstruct.Build(options, flagstruct.WithContinueOnError, func(b *flagstruct.Builder) {
+		b.Name = "hello"
+	})
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		log.Fatalf("hmm: %+v", err)
+	}
+
+	fmt.Printf("parsed: %#+v\n", options)
+}
