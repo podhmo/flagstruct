2b8ee5652d6a571af3db3ee5365f9d45b01da0fe
podhmo <ababjam61+github@gmail.com>
2022-08-05 20:56:44 +0900

    chore: update example

---
+++ examples/06use-with-pflag/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/podhmo/flagstruct"
+	"github.com/spf13/pflag"
+)
+
+type Config struct {
+	Name string `flag:"name"`
+}
+
+var debug bool
+
+func main() {
+	fs := pflag.NewFlagSet("hello", pflag.ExitOnError)
+	fs.BoolVarP(&debug, "debug", "", debug, "debug option")
+
+	config := &Config{Name: "anonymous"}
+
+	binder := &flagstruct.Binder{Config: flagstruct.DefaultConfig()}
+	setenv := binder.Bind(fs, config)
+	if err := setenv(fs); err != nil {
+		panic(err)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.Encode(map[string]interface{}{
+		"debug":  debug,
+		"config": config,
+	})
+}
